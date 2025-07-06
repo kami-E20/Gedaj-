83354dfdcc9d6be551339b971b89cc8f32d26064
kami-E20 <kamieros20@gmail.com>
2025-07-06 11:19:24 +0200

    Add files via upload

---
+++ scripts/generate_film_message.py
@@ -0,0 +1,3 @@
+
+def send_film_of_the_day():
+    print("📽️ Film du jour généré et envoyé avec succès !")
---
+++ scripts/generate_quiz_message.py
@@ -0,0 +1,3 @@
+
+def send_quiz_of_the_day():
+    print("🧠 Quiz du jour généré et envoyé avec succès !")
