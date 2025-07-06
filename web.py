dc12e38bbc0b09d4a43c472a95b2d65330646818
kami-E20 <kamieros20@gmail.com>
2025-07-06 09:01:41 +0200

    Add files via upload

--- web.py
+++ web.py
@@ -1,13 +1,12 @@
-
 from flask import Flask
-import os
+import threading
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Gedaj v1.5 lancé avec succès !"
+    return "Gedaj bot en ligne ✅"
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+def keep_alive():
+    thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
+    thread.start()
