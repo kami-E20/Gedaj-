b8bb5ec92a2e7f6e611efc8bcc55b96b44210714
kami-E20 <kamieros20@gmail.com>
2025-07-06 12:38:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from telebot import types
 from scheduler import run_scheduler
 import threading
+
+# Import des modules de commandes
 from commands.start import register_start
 from commands.help import register_help
 from commands.quiz import register_quiz
@@ -10,6 +12,9 @@ from commands.listener import register_listener
 from commands.classement import register_classement
 from commands.recompenses import register_recompenses
 from commands.abodumois import register_abodumois
+from commands.suggestion import register_suggestion
+from commands.defi import register_defi
+from commands.avis import register_avis
 
 # Charger le token depuis les variables d'environnement
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -23,8 +28,11 @@ register_listener(bot)
 register_classement(bot)
 register_recompenses(bot)
 register_abodumois(bot)
+register_suggestion(bot)
+register_defi(bot)
+register_avis(bot)
 
-# Démarrer la planification dans un thread parallèle
+# Lancer la planification en arrière-plan
 threading.Thread(target=run_scheduler, daemon=True).start()
 
 # Lancer le bot
