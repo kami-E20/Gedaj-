753080069d98767ff524ff6d596d593d4bfdd096
kami-E20 <kamieros20@gmail.com>
2025-07-06 11:41:47 +0200

    Add files via upload

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,9 @@
-
 import schedule
 import time
-from scripts.generate_film_message import send_film_of_the_day
-from scripts.generate_quiz_message import send_quiz_of_the_day
+from scripts.backup import backup_donnees
 
 def run_scheduler():
-    schedule.every().day.at("08:59").do(send_film_of_the_day)
-    schedule.every().day.at("09:00").do(send_quiz_of_the_day)
+    schedule.every().day.at("23:59").do(backup_donnees)
 
     while True:
         schedule.run_pending()
