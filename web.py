dd437cd8a49de24af02f1e357f187075997548e2
kami-E20 <kamieros20@gmail.com>
2025-07-06 07:49:22 +0200

    Add files via upload

--- web.py
+++ web.py
@@ -1,11 +1,1 @@
-from flask import Flask
-from threading import Thread
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Gedaj est en ligne !"
-
-def keep_alive():
-    Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080}).start()
+# web server for uptime
