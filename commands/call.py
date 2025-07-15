c5bb0775ecba211a9325dca2b03955373024cb33
kami-E20 <kamieros20@gmail.com>
2025-07-15 23:25:00 +0200

    Update call.py

--- commands/call.py
+++ commands/call.py
@@ -1,2 +1,12 @@
+from telebot.types import Message
+
+# Liste des ID admin autorisés
+ADMIN_IDS = [5618445554, 879386491]  # Anthony et Kâmį
+
 def register_call(bot):
-    bot.add_command('call', lambda msg: bot.send_message(msg.chat.id, 'Commande /call réservée aux admins exécutée.'))
+    @bot.message_handler(commands=['call'])
+    def handle_call(message: Message):
+        if message.from_user.id in ADMIN_IDS:
+            bot.send_message(message.chat.id, "📞 Appel administratif exécuté avec succès.")
+        else:
+            bot.send_message(message.chat.id, "⛔ Cette commande est réservée aux administrateurs.")
