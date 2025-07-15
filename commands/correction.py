dae88324ff40110f6a355192d3608ff56eeb0f55
kami-E20 <kamieros20@gmail.com>
2025-07-15 22:06:02 +0200

    Update correction.py

--- commands/correction.py
+++ commands/correction.py
@@ -1,7 +1,10 @@
-from telebot import TeleBot
-
-bot = TeleBot('TOKEN')
-
-@bot.message_handler(commands=['correction'])
-def correction(message):
-    bot.send_message(message.chat.id, 'Correction en cours...')
+def register_correction(bot):
+    @bot.message_handler(commands=['correction'])
+    def handle_correction(message):
+        bot.send_message(
+            message.chat.id,
+            "🧐 *Voici la correction du quiz d’aujourd’hui :*\n\n"
+            "📌 Les bonnes réponses s’affichent ici automatiquement après ta participation.\n"
+            "🧠 Continue de jouer chaque jour pour améliorer ton score !",
+            parse_mode="Markdown"
+        )
