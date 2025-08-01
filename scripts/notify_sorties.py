ea5e458310e1d6d0468445211868cc6f5abdc667
kami-E20 <kamieros20@gmail.com>
2025-08-01 21:43:07 +0200

    Update notify_sorties.py

--- scripts/notify_sorties.py
+++ scripts/notify_sorties.py
@@ -1,18 +1,3 @@
-import os
-from telebot import TeleBot
-
-def notify_sorties(bot: TeleBot, sorties, categorie="Sortie"):
-    if not sorties:
-        return
-    admin_ids = [int(x) for x in os.getenv("ADMINS", "879386491,5618445554").split(",")]
-    for s in sorties:
-        texte = (
-            f"🔮 [SORTIE À VENIR] – {s['date']}\n\n"
-            f"🎬 {s['titre']}\n"
-            f"🧠 {s['description'][:250]}...\n"
-            f"🔗 {s['lien']}"
-        )
-        for admin in admin_ids:
-            bot.send_message(admin, texte)
-            if s.get("image"):
-                bot.send_photo(admin, s["image"])
+# TODO : notifier les admins sur les sorties à venir
+def notify_admins_sorties(bot):
+    print("📆 Notification des sorties à venir envoyée aux admins")
