98b7c38f0c2fbe2e3460e82cc82764a0caf2d645
kami-E20 <kamieros20@gmail.com>
2025-08-01 21:47:39 +0200

    Update fetch_anilist_news.py

--- scripts/fetch_anilist_news.py
+++ scripts/fetch_anilist_news.py
@@ -1,31 +1,4 @@
-import requests
-
+# TODO : À implémenter plus tard
 def fetch_anilist_news():
-    query = """
-    {
-      Page(perPage: 1) {
-        media(type: ANIME, sort: TRENDING_DESC) {
-          title {
-            romaji
-          }
-          description(asHtml: false)
-          coverImage {
-            large
-          }
-          siteUrl
-        }
-      }
-    }
-    """
-    url = "https://graphql.anilist.co"
-    response = requests.post(url, json={'query': query})
-    data = response.json()
-    if not data.get("data"):
-        return None
-    media = data["data"]["Page"]["media"][0]
-    return {
-        "titre": media["title"]["romaji"],
-        "description": media["description"],
-        "image": media["coverImage"]["large"],
-        "lien": media["siteUrl"]
-    }
+    print("📰 Récupération des news d'Anilist non encore implémentée.")
+    return []
