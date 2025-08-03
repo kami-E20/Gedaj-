87f8d7a0b3a71db1a9a4200fb03a7f4c6640c659
kami-E20 <kamieros20@gmail.com>
2025-08-03 09:36:12 +0200

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,6 +3,7 @@ from .anniversaires import fetch_anniversaires
 from .backup import backup_donnees
 from .fetch_anilist_news import fetch_anilist_news
 from .fetch_cinema_news import fetch_cinema_news
+
 from .notify_block import (
     publier_film,
     publier_quiz,
@@ -13,7 +14,18 @@ from .notify_block import (
     publier_meilleurs_abonnes,
     publier_abonnes_du_mois
 )
+
 from .notify_sorties import notify_admins_sorties
-from .points import ajouter_points, publier_meilleurs_abonnes as publier_top_points
-from .points_logic import update_points
+from .points import (
+    ajouter_points,
+    publier_meilleurs_abonnes as publier_top_points
+)
+
+from .points_logic import (
+    update_points,
+    sauvegarder_points_utilisateurs,
+    get_points,
+    charger_points
+)
+
 from .publish import run_all
